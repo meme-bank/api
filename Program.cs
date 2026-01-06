28c74974fb0b773c2da6b1373f03cd7044d94a70
Urtyom-Alyanov <mc2017artemklochkcov435@yandex.ru>
2026-01-06 14:53:38 +0300

    Обновление зависимостей и настройка Dependabot для автоматического обновления пакетов.

--- Program.cs
+++ Program.cs
@@ -1,15 +1,20 @@
+using GraphQL;
 using OctopusAPI.Database;
-using GraphQL.Server;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<MeduzaContext>();
-builder.Services.AddControllers();
-builder.Services.AddGraphQL(b => b.AddAutoSchema<MeduzaSchema>().AddSystemTextJson().AddDataLoader());
+// builder.Services.AddControllers();
+builder.Services.AddGraphQL(
+  b => b
+    .AddAutoSchema<MeduzaSchema>()
+    .AddSystemTextJson()
+    .AddDataLoader()
+);
 
 var app = builder.Build();
 
-app.UseGraphQLAltair();
 app.UseHttpsRedirection();
+app.UseGraphQLAltair("/ui/altair");
 
 app.Run();
