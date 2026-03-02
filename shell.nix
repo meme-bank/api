2f388893108fd7962703fb18f4ecb7b8199561c0
Urtyom-Alyanov <urtyomalyanov@gmail.com>
2026-03-02 21:44:45 +0300

    chore: add husky in shell.nix

--- shell.nix
+++ shell.nix
@@ -9,6 +9,14 @@ let
     dotnet-runtime = currentSdk;
     executables = [ "reportgenerator" ];
   };
+  # huskyDotNET = pkgs.buildDotnetGlobalTool {
+  #   pname = "Husky";
+  #   executables = [ "husky" ];
+  #   # dotnet-runtime = currentSdk;
+  #   dotnet-runtime = pkgs.dotnet-sdk_8;
+  #   version = "0.8.0";
+  #   nugetHash = "sha256-WBCOF5aEVjQZRzOwrS4W9TZ6+lSFUEL/Ej+f6RPy+KQ=";
+  # };
 in
 pkgs.mkShell {
   nativeBuildInputs = with pkgs; [
@@ -17,5 +25,7 @@ pkgs.mkShell {
     dotnet-ef
     dotnet-aspnetcore_10
     reportgenerator
+    # huskyDotNET
+    husky
   ];
 }
