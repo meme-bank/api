7a20eb49df33a7cf5a17eaed719d51b17c4cb2f9
Urtyom-Alyanov <urtyomalyanov@gmail.com>
2026-03-02 21:34:57 +0300

    chore: add reportgenerator in shell.nix

--- shell.nix
+++ shell.nix
@@ -1,8 +1,21 @@
 { pkgs ? import <nixpkgs> {} }:
 
+let
+  currentSdk = pkgs.dotnet-sdk_10;
+  reportgenerator = pkgs.buildDotnetGlobalTool {
+    pname = "dotnet-reportgenerator-globaltool";
+    version = "5.5.2";
+    nugetHash = "sha256-5THxbApvqbX08HQ6YtNhtqwmURe67+njmCNLy8RAaJM=";
+    dotnet-runtime = currentSdk;
+    executables = [ "reportgenerator" ];
+  };
+in
 pkgs.mkShell {
   nativeBuildInputs = with pkgs; [
     roslyn-ls
-    dotnet-sdk_10
+    currentSdk
+    dotnet-ef
+    dotnet-aspnetcore_10
+    reportgenerator
   ];
 }
